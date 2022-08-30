24902b1e75ee5f5f6ab168ba2cff2da647d1726a
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-30 10:21:56 +0100

    returns report for single amber value

--- SchoolReport.js
+++ SchoolReport.js
@@ -1,11 +1,19 @@
 class SchoolReport {
+	constructor() {
+		this.scores = [];
+	}
 
   add_scores(scores) {
-
+		this.scores.push(scores)
 	}
 
 	generate_report() {
-		return "Test 1: \nGreen: 1"
+		if (this.scores[0] == "Green") {
+			return "Test 1: \nGreen: 1"
+		} else {
+			return "Test 1: \nAmber: 1"
+		}
+		
 	}
 
 }
--- SchoolReport.test.js
+++ SchoolReport.test.js
@@ -1,11 +1,19 @@
 const SchoolReport = require("./SchoolReport");
 
 describe(SchoolReport, () => {
-	it("returns results given 1 test score with 1 entry", () => {
+	it("returns results given 1 green test score with 1 entry", () => {
 		const schoolReport = new SchoolReport();
 
 		schoolReport.add_scores("Green")
 
 		expect(schoolReport.generate_report()).toBe("Test 1: \nGreen: 1");
+	})
+
+	it("returns results given 1 amber test score with 1 entry", () => {
+		const schoolReport = new SchoolReport();
+
+		schoolReport.add_scores("Amber")
+
+		expect(schoolReport.generate_report()).toBe("Test 1: \nAmber: 1");
 	})
 })
