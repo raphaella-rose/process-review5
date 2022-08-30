aef82d3c1bab4ce04e8dc88c1f7883f06b9b36ef
raphaella-rose <raphaella.rose1@gmail.com>
2022-08-30 10:26:11 +0100

    refactored method for single score in 1 test

--- SchoolReport.js
+++ SchoolReport.js
@@ -8,14 +8,8 @@ class SchoolReport {
 	}
 
 	generate_report() {
-		if (this.scores[0] == "Green") {
-			return "Test 1: \nGreen: 1"
-		} else if (this.scores[0] == "Amber") {
-			return "Test 1: \nAmber: 1"
-		} else {
-			return "Test 1: \nRed: 1"
-		}
 		
+		return `Test 1: \n${this.scores[0]}: 1`
 	}
 
 }
